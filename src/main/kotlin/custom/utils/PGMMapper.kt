6d4b35046b8297dd304e44c54beafa9696c1157e
yuhojin <sdl182975@gmail.com>
2025-02-21 22:43:32 +0900

    Feat: add type

--- src/main/kotlin/custom/utils/PGMMapper.kt
+++ src/main/kotlin/custom/utils/PGMMapper.kt
@@ -6,20 +6,23 @@ import org.economic.statistics.custom.interfaces.PGMService
 import org.economic.statistics.types.const.TOSS
 import org.springframework.stereotype.Component
 import org.economic.statistics.types.const.PGMList
+import org.economic.statistics.types.global.Result
 
 @Component
 class PGMMapper(
     pgmServices: List<PGMService>
 ) {
 
-    private val confirmPay: Map<String, (String) -> Any> = pgmServices
-        .filter { PGMList.isValidPGMType(it.getPGMKey()) }
+    // Map<String, (String) -> Result<String>>
+    // 1. String : 함수 키 이름을 된다.
+    // 2. (String) -> Result<String>  : 파라메터 인자와 return 값이 된다.
+    private val confirmPay: Map<String, (String) -> Result<String>> = pgmServices
         .associateBy(
             keySelector = { it.getPGMKey().lowercase() },
-            valueTransform = { it::confirmPay }
+            valueTransform = { service -> { body -> service.confirmPay(body) } }
         )
 
-    fun <T> executeConfirmPay(pgm: String, body: String): T {
+    fun executeConfirmPay(pgm: String, body: String): Result<String> {
         val lowerPgm = pgm.lowercase()
 
         if (!PGMList.isValidPGMType(lowerPgm)) {
@@ -29,6 +32,6 @@ class PGMMapper(
         val func = confirmPay[lowerPgm]
             ?: throw CustomException(ErrorCode.NotSupportedPGMKeyRequest)
 
-        return func.invoke(body) as T
+        return func.invoke(body)
     }
 }
